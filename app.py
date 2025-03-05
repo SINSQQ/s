fa0e87a039ec22befd65485a2239ca069ff8d50c
SINSQQ <139044396+SINSQQ@users.noreply.github.com>
2025-03-05 15:56:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+import os
 from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
-# كود HTML مدمج داخل Flask
 html_page = """
 <!DOCTYPE html>
 <html lang="ar">
@@ -48,4 +48,5 @@ def home():
     return render_template_string(html_page)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
